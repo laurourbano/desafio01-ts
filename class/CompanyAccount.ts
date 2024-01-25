052c7aaec6e223dae5651f6ad827f19abe680e6f
Lauro Otavio Urbano <lauro@outlook.com.br>
2024-01-25 20:28:27 -0300

    update

--- class/CompanyAccount.ts
+++ class/CompanyAccount.ts
@@ -2,11 +2,7 @@ import { DioAccount } from "./DioAccount"
 
 export class CompanyAccount extends DioAccount {
 
-  constructor(name: string, accountNumber: number){
-    super(name, accountNumber)
-  }
-
   getLoan = (): void => {
-    console.log('Voce pegou um empréstimo')
+    console.log('Você pegou um empréstimo')
   }
 }
